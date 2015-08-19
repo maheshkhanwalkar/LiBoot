3344cc467c742fa12da76b593cd8aaab285ba65e
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2015-08-19 11:03:16 -0400

    Fixed Makefile Dependencies

--- src/entry.c
+++ src/entry.c
@@ -4,9 +4,8 @@
 
 #include <Library/UefiLib.h>
 
-EFI_STATUS EFIAPI uefi_main(IN EFI_HANDLE image_handle, IN 
-		EFI_SYSTEM_TABLE *sys_table)
+EFI_STATUS EFIAPI uefi_main(IN EFI_HANDLE image_handle, IN EFI_SYSTEM_TABLE *sys_table)
 {
-	Print(L"LiBoot (U)EFI Bootloader v.0.01\n");
+	Print(L"LiBoot (U)EFI Bootloader v0.0.1\n");
 	return EFI_SUCCESS;
 }
