0da2503c90033c8d63f64a985f6835c513c82038
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2020-12-10 13:46:35 -0500

    build: install deps using brew on macOS

--- uefi/install_deps.sh
+++ uefi/install_deps.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-sudo apt install nasm g++ build-essential acpica-tools uuid-dev
+# When Linux is detected, assume Ubuntu
+# TODO add more specific checks...
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo apt install nasm g++ build-essential acpica-tools uuid-dev
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install nasm qemu acpica
+else
+    printf "[ERROR] Unknown system. Quitting!\n"
+    exit 1
+fi
+
 git submodule update --init --recursive
